6bbd9eaa933c966eb6619261b3640d8403a2d82b
Jake Beinart <jake.beinart@gmail.com>
2026-03-02 21:34:33 -0600

    add hidden games page and brand styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './components/pages/landing-page/landing-page.component';
+import { GamesPageComponent } from './components/pages/games-page/games-page.component';
 
 export const routes: Routes = [
-  { path: '', component: LandingPageComponent, pathMatch: 'full' },
+  { path: '', component: LandingPageComponent },
+  { path: 'games', component: GamesPageComponent, pathMatch: 'full' },
 ];
